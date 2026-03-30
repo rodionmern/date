86066e9eb756f3575f7734405ff76d51e8c7c7f5
rodionmern <114694164+rodionmern@users.noreply.github.com>
2026-03-30 19:17:36 +0300

    fix: Закомментировал коммиты, т.к. что-то с ними не так теперь, потом может посмотрю как фиксить

--- script.js
+++ script.js
@@ -32,45 +32,11 @@ const result = getDateDifference(now_date, initial_date)
 // Вставляем результат в разметку.
 document.querySelector('#passed_time').innerHTML = result;
 
-const get_data = async () => {
-	// Запрашиваем дату с api github'а, и потом сохраняем её в переменную data.
-	const response = await fetch(`https://api.github.com/users/rodionmern/events/public?_=${Date.now}`)
-		.then((res) => {return res})
-		.catch((err) => console.log(err))
-	const data = response.json()
-
-	// Возращаем дату.
-	return data
-}
-
-// Получаем дату.
-const response = await get_data()
-	.then((res) => {return res})
-	.catch((err) => console.log(err))
-
-let commit_information_h3 = document.querySelector('#commit_info');
-
-// Проверяем тип запроса последнего действия аккаунта, если он равен == "PushEvent", то отдаём информацию о последнем коммите, если нет,
-// то отдаём информацию о том какого типа последнее действие.
-if (response[0].type == 'PushEvent') {
-	// Инициализируем ссылку из запроса, и разделяем её, для удобства составления других ссылок.
-	let commit_splited_url = response[0].repo.url.split('/')
-	let commit_created_at = response[0].created_at
-
-	// Вставляем всё что получили в вёрстку.
-	commit_information_h3.innerHTML = `
-		Последний коммит/пул-реквест/деплой<br><br>
-		был ${new Date(commit_created_at).getDate()}.${new Date(commit_created_at).getMonth()+1}.${new Date(commit_created_at).getFullYear()} в <a href="https://github.com/${commit_splited_url[4]}/${commit_splited_url[5]}">${commit_splited_url[4]}/${commit_splited_url[5]}</a>`;
-} else {
-	// Отдаём в вёрстку информацию о том что последнее событие не связано с коммитами.
-	commit_information_h3.innerHTML = `Последнее событие не связано с коммитами. - ${response[0].type}`
-}
 
 // Вставляем "админку", если в локалсторейже есть мастер-ключик
-if (localStorage.getItem('key') !== null) {
+if (localStorage.getItem('key') != null) {
     document.querySelector('.content').innerHTML += 
         `
-            <hr>
             <div class="admin-block">
                 <h2>админ-функшнс</h2>
                 <div class="admin-btns">
@@ -82,3 +48,37 @@ if (localStorage.getItem('key') !== null) {
 	
 	adminController()
 }
+
+// const get_data = async () => {
+// 	// Запрашиваем дату с api github'а, и потом сохраняем её в переменную data.
+// 	const response = await fetch(`https://api.github.com/users/rodionmern/events/public?_=${Date.now}`)
+// 		.then((res) => {return res})
+// 		.catch((err) => console.log(err))
+// 	const data = response.json()
+
+// 	// Возращаем дату.
+// 	return data
+// }
+
+// // Получаем дату.
+// const response = await get_data()
+// 	.then((res) => {return res})
+// 	.catch((err) => console.log(err))
+
+// let commit_information_h3 = document.querySelector('#commit_info');
+
+// // Проверяем тип запроса последнего действия аккаунта, если он равен == "PushEvent", то отдаём информацию о последнем коммите, если нет,
+// // то отдаём информацию о том какого типа последнее действие.
+// if (response[0].type == 'PushEvent') {
+// 	// Инициализируем ссылку из запроса, и разделяем её, для удобства составления других ссылок.
+// 	let commit_splited_url = response[0].repo.url.split('/')
+// 	let commit_created_at = response[0].created_at
+
+// 	// Вставляем всё что получили в вёрстку.
+// 	commit_information_h3.innerHTML = `
+// 		Последний коммит/пул-реквест/деплой<br><br>
+// 		был ${new Date(commit_created_at).getDate()}.${new Date(commit_created_at).getMonth()+1}.${new Date(commit_created_at).getFullYear()} в <a href="https://github.com/${commit_splited_url[4]}/${commit_splited_url[5]}">${commit_splited_url[4]}/${commit_splited_url[5]}</a>`;
+// } else {
+// 	// Отдаём в вёрстку информацию о том что последнее событие не связано с коммитами.
+// 	commit_information_h3.innerHTML = `Последнее событие не связано с коммитами. - ${response[0].type}`
+// }
